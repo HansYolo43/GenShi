6cc897053b3e9872571e4a624f670dd5f8335e51
hannahomeiza <omeiza81@gmail.com>
2023-11-30 20:28:53 -0500

    including more classes

--- src/app/LoginUseCaseFactory.java
+++ src/app/LoginUseCaseFactory.java
@@ -1,4 +1,76 @@
 package app;
 
+import Entities.CommonUserFactory;
+import Entities.UserFactory;
+import interface_adapter.ViewManagerModel;
+import interface_adapter.logged_in.LoggedInViewModel;
+import interface_adapter.login.LoginController;
+import interface_adapter.login.LoginPresenter;
+import interface_adapter.login.LoginViewModel;
+import use_case.login.LoginInputBoundary;
+import use_case.login.LoginOutputBoundary;
+import use_case.login.LoginInteractor;
+import use_case.login.LoginUserDataAcesssInterface;
+import view.LoginView;
+
+import javax.swing.*;
+import java.io.IOException;
+
 public class LoginUseCaseFactory {
+
+    /** Prevent instantiation. */
+    private LoginUseCaseFactory() {}
+
+    public static LoginView create(
+            ViewManagerModel viewManagerModel,
+            LoginViewModel loginViewModel,
+            LoggedInViewModel loggedInViewModel,
+            LoginUserDataAcesssInterface userDataAccessObject) {
+
+        try {
+            LoginController loginController = createLoginUseCase(viewManagerModel, loginViewModel, loggedInViewModel, userDataAccessObject);
+            return new LoginView(loginViewModel, loginController);
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Could not open user data file.");
+        }
+
+        return null;
+    }
+
+    private static LoginController createLoginUseCase(
+            ViewManagerModel viewManagerModel,
+            LoginViewModel loginViewModel,
+            LoggedInViewModel loggedInViewModel,
+            LoginUserDataAcesssInterface userDataAccessObject) throws IOException {
+
+        // Notice how we pass this method's parameters to the Presenter.
+        LoginOutputBoundary loginOutputBoundary = new LoginPresenter(viewManagerModel, loggedInViewModel, loginViewModel);
+
+        UserFactory userFactory = new CommonUserFactory();
+
+        LoginInputBoundary loginInteractor = new LoginInteractor(
+                userDataAccessObject, loginOutputBoundary);
+
+        return new LoginController(loginInteractor);
+    }
+
+    private static LogoutController createLogoutUseCase(
+            ClearViewModel clearViewModel, ClearUserDataAccessInterface clearUserDataAccessObject)
+            throws IOException {
+
+        // Notice how we pass this method's parameters to the Presenter.
+        ClearOutputBoundary clearOutputBoundary = new ClearPresenter(clearViewModel);
+
+        //new SignupPresenter(viewManagerModel, signupViewModel, loginViewModel);
+
+        UserFactory userFactory = new CommonUserFactory(); // manufacturing users
+
+        ClearInputBoundary clearUserInteractor = new ClearInteractor(
+                clearUserDataAccessObject, clearOutputBoundary, userFactory);
+
+        return new ClearController(clearUserInteractor);
+    }
 }
+
+// add one for log out
+
--- src/app/SignupUseCaseFactory.java
+++ src/app/SignupUseCaseFactory.java
@@ -1,4 +1,59 @@
 package app;
 
-public class SignupUseCaseFactory {
+
+import interface_adapter.login.LoginViewModel;
+import interface_adapter.signup.SignupController;
+import interface_adapter.signup.SignupPresenter;
+import interface_adapter.signup.SignupViewModel;
+import use_case.signup.SignupUserDataAcesssInterface;
+import Entities.CommonUserFactory;
+import Entities.UserFactory;
+import interface_adapter.*;
+import use_case.signup.SignupInputBoundary;
+import use_case.signup.SignupInteractor;
+import use_case.signup.SignupOutputBoundary;
+import view.SignupView;
+
+import javax.swing.*;
+import java.io.IOException;
+
+public class SignupUseCaseFactory { // creates use cases and sends them to use case
+
+    /** Prevent instantiation. */
+    private SignupUseCaseFactory() {}
+
+    public static SignupView create(
+            ViewManagerModel viewManagerModel,
+            LoginViewModel loginViewModel,
+            SignupViewModel signupViewModel,
+            SignupUserDataAcesssInterface userDataAccessObject)
+             {
+
+        try {
+            SignupController signupController = createUserSignupUseCase(viewManagerModel, signupViewModel, loginViewModel, userDataAccessObject);
+
+
+            return new SignupView(signupController, signupViewModel);
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Could not open user data file.");
+        }
+
+        return null;
+    }
+
+    private static SignupController createUserSignupUseCase(ViewManagerModel viewManagerModel, SignupViewModel signupViewModel, LoginViewModel loginViewModel, SignupUserDataAcesssInterface userDataAccessObject) throws IOException {
+
+        // Notice how we pass this method's parameters to the Presenter.
+        SignupOutputBoundary signupOutputBoundary = new SignupPresenter(viewManagerModel, signupViewModel, loginViewModel);
+
+        UserFactory userFactory = new CommonUserFactory();
+
+        SignupInputBoundary userSignupInteractor = new SignupInteractor(
+                userDataAccessObject, signupOutputBoundary, userFactory);
+
+        return new SignupController(userSignupInteractor);
+    }
+
+
 }
+
