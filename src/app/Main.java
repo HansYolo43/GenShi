b77ed82fef16b125f3496687c9b641d02353452a
HansYoloMF <73197756+HansYoloMF@users.noreply.github.com>
2023-11-30 21:11:05 -0500

    Update Main.java

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,7 +6,7 @@ import data_access.FileCardDataAccessObject;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class Sample_Dao_Use {
+public class Main {
     public static void main(String[] args) throws IOException {
 
         String Apikey = "api-key";
